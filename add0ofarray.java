c78dafc566c4418f20083f8a33cddc9f9482f030
Kishorepm <kishorepmofficial@gmail.com>
2016-08-28 01:10:04 +0530

    Add files via upload

---
+++ add0ofarray.java
@@ -0,0 +1,33 @@
+package Hunter_LEVEL;
+
+import java.util.Scanner;
+
+public class add0ofarray {
+	@SuppressWarnings("resource")
+	public static void main(String args[]){
+		int n,m = 0,count=0,c = 0,b=0;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		int[] a=new int[n];
+		Scanner reader=new Scanner(System.in);
+		for(int i=0;i<n;i++)
+		{
+			a[i]=reader.nextInt();
+		}
+		for(int i=0;i<n;i++)
+		{
+			for(int j=i+1;j<n;j++)
+			{
+				if(a[i]+a[j]==0)
+				{
+					b=a[i];
+					c=a[j];
+				}
+			}
+		}
+		System.out.println(b);
+		System.out.println(c);
+		
+	}
+
+}
