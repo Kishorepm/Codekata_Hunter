8c45ebaceda1b92b5fc4710555833ba47251644f
Kishorepm <kishorepmofficial@gmail.com>
2016-08-28 00:46:46 +0530

    Update identicalnumber.java

--- identicalnumber.java
+++ identicalnumber.java
@@ -1,9 +1,11 @@
 package Hunter_LEVEL;
+
 import java.util.Scanner;
+
 public class identicalnumber {
 	@SuppressWarnings("resource")
 	public static void main(String args[]){
-		int n,m = 0;
+		int n,count=0;
 		Scanner s=new Scanner(System.in);
 		n=s.nextInt();
 		int[] a=new int[n];
@@ -14,14 +16,17 @@ public class identicalnumber {
 		}
 		for(int i=0;i<n;i++)
 		{
-			for(int j=0;j<i;j++)
+			for(int j=i+1;j<n;j++)
 			{
 				if(a[i]==a[j])
 				{
-					m=a[i];
+					count=count+1;
 				}
 			}
+			if(count==0)
+	    	 {
+				System.out.println(a[i]);
+	    	 }
 		}
-		System.out.println(m);
 	}
 }
